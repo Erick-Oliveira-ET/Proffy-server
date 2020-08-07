d6b7973dc17482d3890e5d2a5048c25057df4c10
Erick Oliveira <erickoliveirasystem@outlook.com>
2020-08-07 17:39:52 -0300

    Preparing the database
    - Migrations

--- knexfile.ts
+++ knexfile.ts
@@ -2,11 +2,11 @@ import path from 'path';
 
 module.exports = {
     client: 'sqlite3',
-    connections: {
+    connection: {
         filename: path.resolve(__dirname, 'src', 'database', 'database.sqlite'),
     },
     migrations: {
         directory: path.resolve(__dirname, 'src', 'database', 'migrations')
     },
-    useNullAsDefault: true
-}
+    useNullAsDefault: true,
+};
--- src/database/migrations/00_create_users.ts
+++ src/database/migrations/00_create_users.ts
@@ -3,10 +3,10 @@ import Knex from "knex";
 export async function up(knex: Knex){
     return knex.schema.createTable('users', table => {
         table.increments('id').primary();
-        table.increments('name').notNullable();
-        table.increments('avatar').notNullable();
-        table.increments('whatsapp').notNullable();
-        table.increments('bio').notNullable();
+        table.string('name').notNullable();
+        table.string('avatar').notNullable();
+        table.string('whatsapp').notNullable();
+        table.string('bio').notNullable();
     });
 }
 
