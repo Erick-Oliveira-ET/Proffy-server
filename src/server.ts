1a5347d5317babb0cb28bcb6c460fd4e835045ef
Erick Oliveira <erickoliveirasystem@outlook.com>
2020-08-08 09:51:40 -0300

    Install @types/Cors

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,13 @@
 import express from 'express'; //It has to be installed as @types/express
 import routes from './routes';
+import cors from 'cors';
 
 const app = express();
 
 app.use(express.json());
+
+//Alow different ports to access the API
+app.use(cors());
 
 app.use(routes);
 
